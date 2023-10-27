8ce95ceae8d04b17b6e3e12b3cac8958003c83bd
Nikhil Nair <nikhil@cloudflare.com>
2023-10-27 10:50:10 -0500

    Add QinQ (802.1ad) Ethernet header parsing support
    EthernetPdu previously only handled untagged frames and single 802.1Q
    VLAN tags. This adds support for QinQ (802.1ad outer tag + 802.1Q inner
    tag) frames.
    
    EthernetPdu gains a QINQ (0x88A8) constant in the EtherType module, a
    pre-computed header length field stored at construction time rather than
    re-derived on every call to computed_ihl(), a computed_ethertype() method
    that reads the final ethertype after skipping all VLAN tags, and a
    vlan_tags() method returning an iterator over VlanTag structs.
    EthernetPdu::new() now rejects a QINQ outer tag not followed by a DOT1Q
    inner tag with Error::Malformed.
    
    Originally authored by Nikhil Nair and submitted upstream as
    alexforster/pdu#9.
    
    Co-authored-by: Nikhil Nair <nikhil@cloudflare.com>

--- src/ethernet.rs
+++ src/ethernet.rs
@@ -27,6 +27,7 @@ pub mod EtherType {
     pub const IPV4: u16 = 0x0800;
     pub const IPV6: u16 = 0x86DD;
     pub const DOT1Q: u16 = 0x8100;
+    pub const QINQ: u16 = 0x88A8;
     pub const TEB: u16 = 0x6558;
 }
 
@@ -34,6 +35,7 @@ pub mod EtherType {
 #[derive(Debug, Copy, Clone)]
 pub struct EthernetPdu<'a> {
     buffer: &'a [u8],
+    ihl: usize,
 }
 
 /// Contains the inner payload of an [`EthernetPdu`]
@@ -51,15 +53,41 @@ impl<'a> EthernetPdu<'a> {
         if buffer.len() < 14 {
             return Err(Error::Truncated);
         }
-        let pdu = EthernetPdu { buffer };
-        if pdu.tpid() == EtherType::DOT1Q && buffer.len() < 18 {
-            return Err(Error::Truncated);
-        }
-        if pdu.ethertype() < 0x0600 {
+        if u16::from_be_bytes(buffer[12..14].try_into().unwrap()) < 0x0600 {
             // we don't support 802.3 (LLC) frames
             return Err(Error::Malformed);
         }
-        Ok(pdu)
+        let pos = 12;
+        let ethertype = u16::from_be_bytes(buffer[pos..pos + 2].try_into().unwrap());
+        match ethertype {
+            EtherType::DOT1Q => Self::dot1q(buffer, pos + 2),
+            EtherType::QINQ => Self::qinq(buffer, pos + 2),
+            _ => Ok(EthernetPdu { buffer, ihl: 14 }),
+        }
+    }
+
+    fn dot1q(buffer: &'a [u8], pos: usize) -> Result<Self> {
+        // buffer needs the tag + ether len/tag
+        if buffer.len() < pos + 4 {
+            return Err(Error::Truncated);
+        }
+
+        Ok(EthernetPdu { buffer, ihl: pos + 4 })
+    }
+
+    fn qinq(buffer: &'a [u8], mut pos: usize) -> Result<Self> {
+        // buffer needs to contain tag + dot1q tag
+        if buffer.len() < pos + 4 {
+            return Err(Error::Truncated);
+        }
+
+        pos += 2;
+        let ethertype = u16::from_be_bytes(buffer[pos..pos + 2].try_into().unwrap());
+        if ethertype != EtherType::DOT1Q {
+            return Err(Error::Malformed);
+        }
+
+        Self::dot1q(buffer, pos + 2)
     }
 
     /// Returns a reference to the entire underlying buffer that was provided during construction
@@ -100,10 +128,7 @@ impl<'a> EthernetPdu<'a> {
     }
 
     pub fn computed_ihl(&'a self) -> usize {
-        match self.tpid() {
-            EtherType::DOT1Q => 18,
-            _ => 14,
-        }
+        self.ihl
     }
 
     pub fn source_address(&'a self) -> [u8; 6] {
@@ -129,6 +154,10 @@ impl<'a> EthernetPdu<'a> {
         }
     }
 
+    pub fn computed_ethertype(&'a self) -> u16 {
+        u16::from_be_bytes(self.buffer[(self.ihl - 2)..self.ihl].try_into().unwrap())
+    }
+
     pub fn vlan(&'a self) -> Option<u16> {
         match self.tpid() {
             EtherType::DOT1Q => Some(u16::from_be_bytes(self.buffer[14..=15].try_into().unwrap()) & 0x0FFF),
@@ -148,5 +177,213 @@ impl<'a> EthernetPdu<'a> {
             EtherType::DOT1Q => Some(((self.buffer[14] & 0x10) >> 4) > 0),
             _ => None,
         }
+    }
+
+    pub fn vlan_tags(&'a self) -> Option<VlanTagIterator<'a>> {
+        match self.tpid() {
+            EtherType::DOT1Q | EtherType::QINQ => Some(VlanTagIterator { buffer: self.buffer, pos: 12, eol: false }),
+            _ => None,
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    // Helpers to build minimal Ethernet frames as byte vecs.
+    // Layout: dst(6) + src(6) + [vlan tags] + ethertype(2) + payload
+
+    // Minimal valid IPv4 header (20 bytes, no payload): version=4, ihl=5,
+    // total_length=20, protocol=0 (reserved/raw), src=1.2.3.4, dst=5.6.7.8
+    const IPV4_HDR: [u8; 20] = [
+        0x45, 0x00, 0x00, 0x14, // version/ihl, dscp/ecn, total_length
+        0x00, 0x00, 0x00, 0x00, // identification, flags/fragment_offset
+        0x40, 0x00, 0x00, 0x00, // ttl, protocol, checksum
+        0x01, 0x02, 0x03, 0x04, // src
+        0x05, 0x06, 0x07, 0x08, // dst
+    ];
+
+    fn untagged_frame() -> Vec<u8> {
+        let mut f = vec![
+            0x00, 0x11, 0x22, 0x33, 0x44, 0x55, // dst
+            0xaa, 0xbb, 0xcc, 0xdd, 0xee, 0xff, // src
+            0x08, 0x00, // ethertype: IPv4
+        ];
+        f.extend_from_slice(&IPV4_HDR);
+        f
+    }
+
+    fn dot1q_frame(vlan_id: u16) -> Vec<u8> {
+        let mut f = vec![
+            0x00, 0x11, 0x22, 0x33, 0x44, 0x55, // dst
+            0xaa, 0xbb, 0xcc, 0xdd, 0xee, 0xff, // src
+            0x81, 0x00, // TPID: DOT1Q
+        ];
+        f.extend_from_slice(&vlan_id.to_be_bytes()); // TCI: PCP=0, DEI=0, VID
+        f.extend_from_slice(&[0x08, 0x00]); // inner ethertype: IPv4
+        f.extend_from_slice(&IPV4_HDR);
+        f
+    }
+
+    fn qinq_frame(outer_vid: u16, inner_vid: u16) -> Vec<u8> {
+        let mut f = vec![
+            0x00, 0x11, 0x22, 0x33, 0x44, 0x55, // dst
+            0xaa, 0xbb, 0xcc, 0xdd, 0xee, 0xff, // src
+            0x88, 0xa8, // TPID: QINQ
+        ];
+        f.extend_from_slice(&outer_vid.to_be_bytes()); // outer TCI
+        f.extend_from_slice(&[0x81, 0x00]); // inner TPID: DOT1Q
+        f.extend_from_slice(&inner_vid.to_be_bytes()); // inner TCI
+        f.extend_from_slice(&[0x08, 0x00]); // ethertype: IPv4
+        f.extend_from_slice(&IPV4_HDR);
+        f
+    }
+
+    #[test]
+    fn test_untagged() {
+        let frame = untagged_frame();
+        let pdu = EthernetPdu::new(&frame).unwrap();
+
+        assert_eq!(pdu.computed_ihl(), 14);
+        assert_eq!(pdu.tpid(), EtherType::IPV4);
+        assert_eq!(pdu.ethertype(), EtherType::IPV4);
+        assert_eq!(pdu.computed_ethertype(), EtherType::IPV4);
+        assert!(pdu.vlan_tags().is_none());
+        assert_eq!(pdu.as_bytes(), &frame[..14]);
+        assert!(matches!(pdu.into_inner().unwrap(), Ethernet::Ipv4(_)));
+    }
+
+    #[test]
+    fn test_dot1q() {
+        let frame = dot1q_frame(0x0064); // VLAN 100
+        let pdu = EthernetPdu::new(&frame).unwrap();
+
+        assert_eq!(pdu.computed_ihl(), 18);
+        assert_eq!(pdu.tpid(), EtherType::DOT1Q);
+        assert_eq!(pdu.ethertype(), EtherType::IPV4);
+        assert_eq!(pdu.computed_ethertype(), EtherType::IPV4);
+        assert_eq!(pdu.vlan(), Some(0x0064));
+
+        let tags: Vec<VlanTag> = pdu.vlan_tags().unwrap().collect();
+        assert_eq!(tags.len(), 1);
+        assert_eq!(tags[0].protocol_id, EtherType::DOT1Q);
+        assert_eq!(tags[0].id, 0x0064);
+
+        assert_eq!(pdu.as_bytes(), &frame[..18]);
+        assert!(matches!(pdu.into_inner().unwrap(), Ethernet::Ipv4(_)));
+    }
+
+    #[test]
+    fn test_qinq() {
+        let frame = qinq_frame(0x000a, 0x0064); // outer VLAN 10, inner VLAN 100
+        let pdu = EthernetPdu::new(&frame).unwrap();
+
+        assert_eq!(pdu.computed_ihl(), 22);
+        assert_eq!(pdu.tpid(), EtherType::QINQ);
+        assert_eq!(pdu.computed_ethertype(), EtherType::IPV4);
+
+        let tags: Vec<VlanTag> = pdu.vlan_tags().unwrap().collect();
+        assert_eq!(tags.len(), 2);
+        assert_eq!(tags[0].protocol_id, EtherType::QINQ);
+        assert_eq!(tags[0].id, 0x000a);
+        assert_eq!(tags[1].protocol_id, EtherType::DOT1Q);
+        assert_eq!(tags[1].id, 0x0064);
+
+        assert_eq!(pdu.as_bytes(), &frame[..22]);
+        // into_inner() dispatches on tpid() not computed_ethertype(), so a QinQ
+        // frame with an unrecognised outer TPID (0x88A8) yields Ethernet::Raw
+        assert!(matches!(pdu.into_inner().unwrap(), Ethernet::Raw(_)));
+    }
+
+    #[test]
+    fn test_qinq_payload_offset() {
+        // Verify that into_inner() slices from byte 22 onward, not byte 14 or 18
+        let frame = qinq_frame(0x000a, 0x0064);
+        let pdu = EthernetPdu::new(&frame).unwrap();
+        let payload = &frame[22..]; // ethertype(2) already consumed, rest is IPv4 onward
+                                    // into_inner slices from computed_ihl which is 22
+        assert_eq!(&pdu.buffer()[pdu.computed_ihl()..], payload);
+    }
+
+    #[test]
+    fn test_truncated_untagged() {
+        // 13 bytes - one short of minimum
+        let frame = &untagged_frame()[..13];
+        assert_eq!(EthernetPdu::new(frame).unwrap_err(), Error::Truncated);
+    }
+
+    #[test]
+    fn test_truncated_dot1q() {
+        // Has DOT1Q TPID but not enough bytes for the tag
+        let frame = &dot1q_frame(0x0064)[..15];
+        assert_eq!(EthernetPdu::new(frame).unwrap_err(), Error::Truncated);
+    }
+
+    #[test]
+    fn test_truncated_qinq() {
+        // Has QINQ TPID but not enough bytes for both tags
+        let frame = &qinq_frame(0x000a, 0x0064)[..17];
+        assert_eq!(EthernetPdu::new(frame).unwrap_err(), Error::Truncated);
+    }
+
+    #[test]
+    fn test_malformed_qinq_no_dot1q() {
+        // QINQ outer tag not followed by DOT1Q - should be Malformed
+        let mut frame = qinq_frame(0x000a, 0x0064);
+        // Overwrite the inner TPID (bytes 16..18) with something that is not DOT1Q
+        frame[16] = 0x08;
+        frame[17] = 0x00;
+        assert_eq!(EthernetPdu::new(&frame).unwrap_err(), Error::Malformed);
+    }
+
+    #[test]
+    fn test_malformed_llc_frame() {
+        // ethertype < 0x0600 signals an 802.3 LLC frame, which is not supported
+        let mut frame = untagged_frame();
+        frame[12] = 0x05;
+        frame[13] = 0xff;
+        assert_eq!(EthernetPdu::new(&frame).unwrap_err(), Error::Malformed);
+    }
+}
+
+/// Represents a VLAN tag
+#[derive(Debug, Copy, Clone)]
+pub struct VlanTag {
+    pub protocol_id: u16,
+    pub priority_codepoint: u8,
+    pub drop_eligible: bool,
+    pub id: u16,
+}
+
+#[derive(Debug, Copy, Clone)]
+pub struct VlanTagIterator<'a> {
+    buffer: &'a [u8],
+    pos: usize,
+    eol: bool,
+}
+
+impl<'a> Iterator for VlanTagIterator<'a> {
+    type Item = VlanTag;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.eol {
+            return None;
+        }
+        let tpid = u16::from_be_bytes(self.buffer[self.pos..self.pos + 2].try_into().unwrap());
+        if tpid != EtherType::DOT1Q && tpid != EtherType::QINQ {
+            return None;
+        }
+        if tpid == EtherType::DOT1Q {
+            self.eol = true;
+        }
+        let vlan_tag = VlanTag {
+            protocol_id: tpid,
+            priority_codepoint: (self.buffer[self.pos + 2] & 0xE0) >> 5,
+            drop_eligible: ((self.buffer[self.pos + 2] & 0x10) >> 4) > 0,
+            id: u16::from_be_bytes([self.buffer[self.pos + 2] & 0x0F, self.buffer[self.pos + 3]]),
+        };
+        self.pos += 4;
+        Some(vlan_tag)
     }
 }
